8b8cc34b8688bc3d5831fcd9bbfa4e1c5d6de9d2
Sanaz Hosseini <sanazhosseini73@outlook.com>
2025-03-16 00:32:15 +0100

    fix: change default values in user creation

--- backend/src/main/java/com/gisma/mentorship_network/service/UserService.java
+++ backend/src/main/java/com/gisma/mentorship_network/service/UserService.java
@@ -1,13 +1,16 @@
 package com.gisma.mentorship_network.service;
 import com.gisma.mentorship_network.model.User;
 import com.gisma.mentorship_network.repository.UserRepository;
+import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -53,7 +56,8 @@ public class UserService {
             @Size(min = 4, max = 12, message = "Password must be between 4 and 12 characters")
             String password,
 
-            boolean is_mentor
+            Boolean is_mentor,
+            Boolean is_mentee
     ) {}
 
     public User createUser(CreateUserRequest request) {
@@ -67,7 +71,10 @@ public class UserService {
         user.setLast_name(request.last_name);
         user.setEmail(request.email);
         user.setPassword(request.password);
-        user.setIs_mentor(request.is_mentor);
+        user.setIs_mentor(Optional.ofNullable(request.is_mentor).orElse(false));
+        user.setIs_mentee(Optional.ofNullable(request.is_mentee).orElse(true));
+        user.setCreated_at(LocalDateTime.now());
+        user.setIs_active(true);
 
         return userRepository.save(user);
     }
