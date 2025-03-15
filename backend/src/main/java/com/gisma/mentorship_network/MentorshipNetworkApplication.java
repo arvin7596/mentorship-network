e27beb56a7c8f1dc2b4e0e5b185b006b705a49a5
Sanaz Hosseini <sanazhosseini73@outlook.com>
2025-03-15 11:37:11 +0100

    feat: initial backend side

---
+++ backend/src/main/java/com/gisma/mentorship_network/MentorshipNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.gisma.mentorship_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorshipNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorshipNetworkApplication.class, args);
+	}
+
+}
