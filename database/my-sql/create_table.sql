337387d671d303e20407a508cf5ed094989b2e75
Arvin Ghaffari <arvinghaffari75@gmail.com>
2025-03-15 12:10:56 +0100

    feat : create database tables

---
+++ database/my-sql/create_table.sql
@@ -0,0 +1,59 @@
+CREATE TABLE Users (
+	user_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    password VARCHAR(255),
+    phone VARCHAR(50),
+    birth_date DATE,
+    location VARCHAR(50),
+    is_mentor BOOLEAN,
+    is_mentee BOOLEAN,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    is_active BOOLEAN
+);
+
+CREATE TABLE User_Skills (
+	skill_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+	skill_name VARCHAR(50),
+    skill_level ENUM('beginner', 'intermediate','advanced')
+);
+
+
+CREATE TABLE Mentor_Availability (
+ mentor_id INT,
+ FOREIGN KEY (mentor_id) REFERENCES Users(user_id),
+ weekday ENUM('monday', 'tuesday','wednesday' , 'thursday' , 'friday' , 'saturday' , 'sunday'),
+ start_time DATETIME,
+ end_date DATETIME
+);
+
+
+CREATE TABLE Mentorship_Match(
+match_id INT PRIMARY KEY AUTO_INCREMENT,
+mentor_id INT,
+ FOREIGN KEY (mentor_id) REFERENCES Users(user_id),
+ mentee_id INT,
+ FOREIGN KEY (mentee_id) REFERENCES Users(user_id),
+ topic VARCHAR(50),
+ status ENUM('new', 'in progress','completed','canceled', 'on hold'),
+ progress INT CHECK (progress >= 0 AND progress <= 100),
+ mentor_feedback TEXT,
+ mentee_feedback TEXT
+);
+
+CREATE TABLE Mentorship_Session(
+  session_id INT PRIMARY KEY AUTO_INCREMENT,
+  mentorship_match_id INT,
+  FOREIGN KEY (mentorship_match_id) REFERENCES Mentorship_Network(match_id),
+  status ENUM("pending", "accepted", "completed", "canceled"),
+  scheduled_date DATETIME,
+  mentor_notes TEXT
+); 
+
+CREATE TABLE Session_Feedback (
+session_id,
+FOREIGN KEY (session_id) REFERENCES Mentorship_Session(session_id),
+);
