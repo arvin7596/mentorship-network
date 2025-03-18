eb36a07ec37d5be6233daeb00cd7e50b96c5c317
Sanaz Hosseini <sanazhosseini73@outlook.com>
2025-03-18 19:16:21 +0100

    feat: add exption handler and add delete and update api

--- backend/src/main/java/com/gisma/mentorship_network/service/UserService.java
+++ backend/src/main/java/com/gisma/mentorship_network/service/UserService.java
@@ -1,16 +1,16 @@
 package com.gisma.mentorship_network.service;
+
 import com.gisma.mentorship_network.model.User;
 import com.gisma.mentorship_network.repository.UserRepository;
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -58,7 +58,8 @@ public class UserService {
 
             Boolean is_mentor,
             Boolean is_mentee
-    ) {}
+    ) {
+    }
 
     public User createUser(CreateUserRequest request) {
         if (userRepository.existsByEmail(request.email)) {
@@ -77,6 +78,62 @@ public class UserService {
         user.setIs_active(true);
 
         return userRepository.save(user);
+    }
+
+
+    public record UpdateUserRequest(
+            @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
+            String first_name,
+
+            @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
+            String last_name,
+
+            @Email(message = "Invalid email format")
+            String email,
+
+            Boolean is_mentor,
+            Boolean is_mentee,
+            String country,
+            String city,
+            String phone,
+            Date birth_date
+    ) {
+    }
+
+    public User updateUser(Integer id, UpdateUserRequest user) {
+        if (!userRepository.existsById(id)) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "User with ID " + id + " not found.");
+        }
+
+        User userToUpdate = userRepository.findById(id).orElse(null);
+
+        Optional.ofNullable(user.first_name).ifPresent(userToUpdate::setFirst_name);
+        Optional.ofNullable(user.last_name).ifPresent(userToUpdate::setLast_name);
+        Optional.ofNullable(user.email).ifPresent(email -> {
+            if (userRepository.existsByEmail(user.email)) {
+                throw new ResponseStatusException(
+                        HttpStatus.BAD_REQUEST, "Email is already in use");
+            }
+            userToUpdate.setEmail(email);
+        });
+        Optional.ofNullable(user.is_mentor).ifPresent(userToUpdate::setIs_mentor);
+        Optional.ofNullable(user.is_mentee).ifPresent(userToUpdate::setIs_mentee);
+        Optional.ofNullable(user.country).ifPresent(userToUpdate::setCountry);
+        Optional.ofNullable(user.city).ifPresent(userToUpdate::setCity);
+        Optional.ofNullable(user.phone).ifPresent(userToUpdate::setPhone);
+        Optional.ofNullable(user.birth_date).ifPresent(userToUpdate::setBirth_date);
+
+        return userRepository.save(userToUpdate);
+
+    }
+
+    public void deleteUser(Integer id) {
+        if (!userRepository.existsById(id)) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "User with ID " + id + " not found.");
+        }
+        userRepository.deleteById(id);
     }
 
 }
