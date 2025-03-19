e8f4e9b9403e4db14eb8c54aea10640ce5902a40
Arvin Ghaffari <arvinghaffari75@gmail.com>
2025-03-19 20:55:56 +0100

    feat : add procedures calls

---
+++ database/my-sql/procedures_calls.sql
@@ -0,0 +1,6 @@
+CALL RegisterUser('Test', 'Test', 'Test@example.com', 'test_password', '1122334455', '1996-06-16', 'Iran' , 'Tehran', TRUE, FALSE);
+CALL AssignSkill(1, 'Java Programming', 'advanced'); -- (user_id, skill_name, skill_level)
+CALL CreateMentorshipMatch(2, 5, 'Data Science'); -- (mentor_id, mentee_id, topic) 
+CALL ScheduleMentorshipSession(3, '2025-04-01 14:00:00'); -- (mentorship_match_id, scheduled_date)
+CALL UpdateSessionStatus(1, 'completed'); -- (match_id , session_status) 
+CALL AddSessionFeedback(1, 'The session was very helpful!', 9.5); -- (session_id, mentee_feedback, rate)
