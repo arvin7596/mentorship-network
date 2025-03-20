88c0bbba4837e27641b9663c00a401ce85f25fb8
Sanaz Hosseini <sanazhosseini73@outlook.com>
2025-03-20 13:41:43 +0100

    feat: add insert skill api and its validation

--- backend/src/main/java/com/gisma/mentorship_network/service/SkillService.java
+++ backend/src/main/java/com/gisma/mentorship_network/service/SkillService.java
@@ -1,8 +1,11 @@
 package com.gisma.mentorship_network.service;
 
 import com.gisma.mentorship_network.model.Skill;
+import com.gisma.mentorship_network.model.SkillLevel;
 import com.gisma.mentorship_network.repository.SkillRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -16,5 +19,19 @@ public class SkillService {
 
     public List<Skill> getAllUserSkills(Long userId) {
         return skillRepository.getUserSkills(userId);
+    }
+
+    public record CreateSkillRequest(String skill_name, Long user_id, SkillLevel skill_level) {}
+    public Skill createUserSkill(CreateSkillRequest request) {
+        if (skillRepository.existsByNameAndUser(request.skill_name, request.user_id)) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "Skill already existed");
+        }
+
+        Skill skill = new Skill();
+        skill.setSkill_name(request.skill_name);
+        skill.setSkill_level(request.skill_level);
+        skill.setUser_id(request.user_id);
+        return skillRepository.save(skill);
     }
 }
