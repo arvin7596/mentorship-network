cd4653d6ec98035fd67ef88611015e85f177d857
Arvin Ghaffari <arvinghaffari75@gmail.com>
2025-03-27 13:06:27 +0330

    fix : change post api response types and add user dto type

--- backend/src/main/java/com/gisma/mentorship_network/controller/PostController.java
+++ backend/src/main/java/com/gisma/mentorship_network/controller/PostController.java
@@ -2,6 +2,8 @@ package com.gisma.mentorship_network.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gisma.mentorship_network.model.Post;
+import com.gisma.mentorship_network.model.PostDTO;
 import com.gisma.mentorship_network.service.PostService;
 
 @RestController
@@ -33,17 +36,21 @@ public class PostController {
     }
 
     @PostMapping
-    public Post createPost(@RequestBody PostService.CreatePostRequest request) {
-        return postService.createPost(request);
+    public ResponseEntity<PostDTO> createPost(@RequestBody PostService.CreatePostRequest request) {
+        PostDTO createdPost = postService.createPost(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdPost);
     }
 
     @PutMapping("/{id}")
-    public Post updatePost(@PathVariable Long id, @RequestBody PostService.UpdatePostRequest request) {
-        return postService.updatePost(id, request);
+    public ResponseEntity<Post> updatePost(@PathVariable Long id, @RequestBody PostService.UpdatePostRequest request) {
+        Post updatedPost = postService.updatePost(id, request);
+        return ResponseEntity.ok(updatedPost);
     }
     
     @DeleteMapping("/{id}")
-    public void deletePost(@PathVariable Long id) {
+    public ResponseEntity<Void> deletePost(@PathVariable Long id) {
         postService.deletePost(id);
+        return ResponseEntity.noContent().build();
+        
     }  
 }
